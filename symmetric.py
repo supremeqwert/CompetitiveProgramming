dd8db35b6e94b342f0587a311ff465041aa52f87
supremeqwert <dumarscourt@gmail.com>
2021-02-11 17:45:16 -0600

    symmetric exit ...

---
+++ symmetric.py
@@ -0,0 +1,12 @@
+import sys
+while True:
+    a = input();
+    later = []
+    for i in range(a):
+        if i%2 == 1:
+            print(input())
+        else :
+            later.append(input())
+    print(later.reverse())
+
+    
